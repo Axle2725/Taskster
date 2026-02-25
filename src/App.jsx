93f8dff2826f2ecf7fe0105df8210dcc3d840ef2
Ayomide Jaiyesimi <ayomidejai0@gmail.com>
2026-02-25 13:53:12 +0100

    src/components/Form.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,12 @@
-import { useEffect, useState } from "react";
-import ProgressTracker from "./components/ProgressTracker";
-import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
+import { Route, Routes } from "react-router-dom";
 import "./Style.css";
+import Form from "./components/Form";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  });
-
-  const addTask = (task) => {
-    setTasks([...tasks, task]);
-  };
-
-  const deleteTask = (index) => {
-    setTasks(tasks.filter((_, i) => i != index));
-  };
-
-  const updateTask = (updatedTask, index) => {
-    const newtask = [...tasks];
-    newtask[index] = updatedTask;
-    setTasks(newtask);
-  };
-
-  const clearTasks = () => {
-    setTasks([]);
-  };
-
   return (
-    <div className="App">
-      <header>
-        <h1 className="title">Taskster</h1>
-        <p className="tagline">Your Friendly Task Manager</p>
-      </header>
-      <TaskForm addTask={addTask} />
-      <TaskList
-        className=""
-        tasks={tasks}
-        updateTask={updateTask}
-        deleteTask={deleteTask}
-      />
-      <ProgressTracker tasks={tasks} />
-
-      {tasks.length > 0 && (
-        <button className="clear-btn" onClick={clearTasks}>
-          Clear All Tasks
-        </button>
-      )}
-    </div>
+    <Routes>
+      <Route path="/" element={<Form />} />
+    </Routes>
   );
 }
 
