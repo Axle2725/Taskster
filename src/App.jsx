9d647ba884f98aafff291f75e88c6b9be22bd915
Ayomide Jaiyesimi <ayomidejai0@gmail.com>
2026-03-11 09:47:13 +0100

    Removed changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import "./Style.css";
 import Form from "./pages/Form";
 import Register from "./pages/Register";
 import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/register" element={<Register />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       <Route path="/taskpage" element={<Form />} />
     </Routes>
   );
