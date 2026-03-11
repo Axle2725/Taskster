08d55ec76463acec359007fa1129772dbd722ed3
Ayomide Jaiyesimi <ayomidejai0@gmail.com>
2026-03-11 09:24:51 +0100

    Dashboard addition

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,9 +1,33 @@
+import ProgressTracker from "../components/ProgressTracker";
 import TaskForm from "../components/TaskForm";
+import TaskList from "../components/TaskList";
+import { useEffect, useState } from "react";
 
-export default function Form({ setTasks, tasks }) {
+export default function Form() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
   const addTask = (task) => {
     setTasks([...tasks, task]);
   };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index));
+  };
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  };
+
+  const clearTasks = () => {
+    setTasks([]);
+  };
+
   return (
     <div className="App">
       <header>
@@ -11,6 +35,19 @@ export default function Form({ setTasks, tasks }) {
         <p className="tagline">Your Friendly Task Manager</p>
       </header>
       <TaskForm addTask={addTask} />
+      <TaskList
+        className=""
+        tasks={tasks}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+      />
+      <ProgressTracker tasks={tasks} />
+
+      {tasks.length > 0 && (
+        <button className="clear-btn" onClick={clearTasks}>
+          Clear All Tasks
+        </button>
+      )}
     </div>
   );
 }
